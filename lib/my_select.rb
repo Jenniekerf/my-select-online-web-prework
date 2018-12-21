e664aa3977d7a215539b70f9bffd20a9891e7009
Jennie Kerfstedt <jenniekerf@gmail.com>
2018-12-21 01:06:17 +0000

    Automatically backed up by Learn

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -1,10 +1,16 @@
 def my_select(collection)
  i = 0 
  empty_array = []
+ if collection.length > 0
  while i < collection.length 
- empty_array << yield(collection[i])
-i += 1 
-end
-empty_array
-end
-
+ if yield(collection[i]) == true
+     empty_array << collection[i]
+   end
+   i += 1 
+ end
+ end
+ empty_array
+ end
+     
+     
+     
